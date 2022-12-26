e70459ba85853769dd98edb9df81e486293ced10
unknown <fahimismail75.scc@gmail.com>
2022-12-26 12:24:01 +0600

    almost-done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { CartComponent } from './cart/cart.component';
 import { CreateProductComponent } from './create-product/create-product.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DataTableComponent } from './data-table/data-table.component';
-import { HomeComponent } from './home/home.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
@@ -13,7 +13,7 @@ const routes: Routes = [
   { path: 'products', component: DataTableComponent },
   { path: 'product/:id', component: ProductComponent },
   { path: 'products/create', component: CreateProductComponent },
-  { path: 'products/edit/:id', component: CartComponent },
+  { path: 'products/edit/:id', component: ProductEditComponent },
   { path: 'my-cart', component: CartComponent }
 ];
 
